185195495c6fbee8d2fe8dd9fe12c46c44c09e87
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-02-24 20:39:51 +0100

    base

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+THEMES = {
+    "classic-theme": {
+        "primary": "#FFFFFF",
+        "secondary": "#000000",
+        "accent": "#E03436",
+        "contrast": "#333333",
+        "tertiary": "#648381"
+    },
+    "democratic-galaxy-theme": {
+        "primary": "#ffb400",
+        "secondary": "#ba6d00",
+        "background": "#222323",
+        "tertiary": "#686868",
+        "contrast": "#0092a6"
+    },
+    "cyberpunk-theme": {
+        "primary": "#000000",
+        "secondary": "#c5003c",
+        "accent": "#880425",
+        "contrast": "#f3e600",
+        "tertiary": "#55ead4"
+    },
+}
